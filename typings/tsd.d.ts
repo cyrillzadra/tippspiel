34d0eda691c562cc262415aa668d5ed32c990602
Cyrill Zadra <cyrill.zadra@tie.ch>
2016-02-27 12:50:45 +0100

    added cordova typings currently not used

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="cordova/cordova.d.ts" />
+/// <reference path="cordova/plugins/Device.d.ts" />
+/// <reference path="cordova/plugins/Globalization.d.ts" />
+/// <reference path="cordova/plugins/InAppBrowser.d.ts" />
